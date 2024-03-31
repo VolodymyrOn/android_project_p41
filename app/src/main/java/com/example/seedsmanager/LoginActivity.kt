bb0c58b11688f6999bf82617579d3c27619756b3
VolodymyrOn <saharokword@gmail.com>
2024-03-31 22:06:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/seedsmanager/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.seedsmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}/*git remote add origin https://github.com/VolodymyrOn/android_project_p41.git
+git branch -M main
+git push -u origin main*/
